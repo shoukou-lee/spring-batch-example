8517b54bc6367857f1c8ada58f026cc4ab7e9a32
shoukou-lee <lee_seungho@inha.edu>
2022-03-02 10:16:31 +0900

    fix: query-dsl 유닛테스트 시나리오 수정

--- src/test/java/com/shoukou/springbatchexample/repository/TeacherRepositoryTest.java
+++ src/test/java/com/shoukou/springbatchexample/repository/TeacherRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -37,52 +38,58 @@ public class TeacherRepositoryTest {
     @Test
     void findAllTeacher_성공 () {
         // given
+        List<Teacher> teachers = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             Teacher t = Teacher.builder()
                     .name("Teacher" + Long.valueOf(i))
                     .subject("Subject" + Long.valueOf(i))
                     .build();
-            Teacher ret = teacherRepository.save(t);
+            teachers.add(t);
         }
-        List<Teacher> teachers = teacherRepository.findAll();
+        teacherRepository.saveAll(teachers);
 
         // when - 얘는 Student 정보도 바로 fetch join
         List<Teacher> result = teacherRepository.findAllTeachersWithStudents();
 
         // then
-        for (int i = 0; i < teachers.size(); i++) {
+        for (int i = 0; i < result.size(); i++) {
             assertThat(result.get(i).getId()).isEqualTo(teachers.get(i).getId());
             assertThat(result.get(i).getName()).isEqualTo(teachers.get(i).getName());
             assertThat(result.get(i).getSubject()).isEqualTo(teachers.get(i).getSubject());
         }
     }
 
-    @Transactional
     @Test
     void findTeacherByStudentId_성공() {
         // given
-        for (int i = 0; i < 2; i++) {
-            Teacher t = Teacher.builder()
-                    .name("Teacher" + Long.valueOf(i))
-                    .subject("Subject" + Long.valueOf(i))
-                    .build();
-            teacherRepository.save(t);
-        }
+        List<Teacher> teachers = new ArrayList<>();
+        List<Student> students = new ArrayList<>();
 
         for (int i = 0; i < 4; i++) {
             Student s = Student.builder()
                     .name("Student" + Long.valueOf(i))
                     .build();
-            studentRepository.save(s);
+            students.add(s);
         }
 
-        List<Teacher> teachers = teacherRepository.findAllTeachersWithStudents();
-        List<Student> students = studentRepository.findAll();
+        for (int i = 0; i < 2; i++) {
+            Teacher t = Teacher.builder()
+                    .name("Teacher" + Long.valueOf(i))
+                    .subject("Subject" + Long.valueOf(i))
+                    .build();
+            teachers.add(t);
+        }
 
         teachers.get(0).addStudent(students.get(0));
         teachers.get(0).addStudent(students.get(1));
         teachers.get(1).addStudent(students.get(2));
         teachers.get(1).addStudent(students.get(3));
+
+        teacherRepository.saveAll(teachers);
+        studentRepository.saveAll(students);
+
+        List<Teacher> retTeachers = teacherRepository.findAllTeachersWithStudents();
+        List<Student> retStudents = studentRepository.findAll();
 
         // when
         Teacher teacher0 = teacherRepository.findTeacherByStudentId(1L)
